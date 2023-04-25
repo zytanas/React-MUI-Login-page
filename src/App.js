c72cce817512a26cbb0fb68ec496cb773703bdb4
hailzytanas <92881119+zytanas@users.noreply.github.com>
2023-04-25 13:37:19 +0800

    #1 excluded "create new acc"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import './App.css';
-import Registration from './components/Registration';
+import { Grid,createTheme, ThemeProvider, } from '@mui/material';
 import Login from './components/Login';
+
+
+const mytheme = createTheme({
+  palette: {
+
+      mode: 'dark',
+
+      primary: {
+          main: '#00000',
+          contrastText: '#ffffff',
+      },
+      secondary: {
+          main: '#1b232e',
+      },
+      text: {
+          primary: '#ffffff',
+          fontSize: 22,
+          fontWeight: 'bold'
+      },
+      background: {
+          paper: '#11171f',
+      },
+      action: {
+          hover: '#ba5b5b',
+      },
+  },
+});
+
 
 function App() {
   return (
+    <ThemeProvider theme={mytheme}>
     <div className="App">
      <Login/>
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import { Grid, Paper, TextField, Divider, Button, Typography, Avatar, Box} from '@mui/material'
-import SignUp from './SignUp'
 const Login = () => {
-    const paperStyle = { padding: 40, height: '40vh', width: 350, borderRadius: '16px', margin: 'auto', backgroundColor: 'maroon' }
-    const avatarStyle = { backgroundColor: 'maroon'}
-    const btnStyle = { margin: '8px 0', fontWeight:600, backgroundColor: '#e00202', color: 'white' }
-    const tfStyle = { backgroundColor: '#ededed',borderRadius: '16px' }
-    const h2Style={color:'white'}
+    const paperStyle = { padding: 40, height: '35vh', width: 350, borderRadius: '16px', margin: 'auto', backgroundColor: '#191e33' }
+    
     
     return (
         <Box sx={{ bgcolor: '#262626', height: '100vh', width: '100vw' }}>
         <Grid>
-            <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={5} style={paperStyle}>
                 <Grid align='center'>
-                <Avatar style={avatarStyle} variant='square' src='https://newmediaservices.com.au/wp-content/uploads/2019/02/NMS-LOGO-HD-2014_1.png'sx={{ width: 130, height: 56 }}>     
+                <Avatar  variant='square' src='https://newmediaservices.com.au/wp-content/uploads/2019/02/NMS-LOGO-HD-2014_1.png'sx={{ width: 130, height: 56 }}>     
                 </Avatar>
         
-                    <h2 style={h2Style}>SIGN IN</h2>
+                    <h2 >SIGN IN</h2>
                 </Grid>
-                <TextField placeholder='Username' style={tfStyle} fullWidth required />
+                <TextField placeholder='Username' sx={{bgcolor:"#24262e"}} fullWidth required />
                 <Divider />
                 <Divider />
                 <Divider />
-                <TextField placeholder='Password' style={tfStyle} type='password' fullWidth required />
+                <TextField placeholder='Password' type='password' sx={{bgcolor:"#24262e"}} fullWidth required />
 
-                <Button type='submit' variant='contained' style={btnStyle} fullWidth  >
+                <Button type='submit' variant='contained'fullWidth 
+                sx={{
+                    fontWeight: 'bold',
+                    bgcolor: '#1A4D2E', ":hover": {
+                        bgcolor: "#7a0702",
+                        color: "white"
+                    }
+                }}>
                     Log In
                 </Button>
 
