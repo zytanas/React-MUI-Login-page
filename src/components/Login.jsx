4f211c3b65e3b4285245305b58b9a14d3c93acd8
hailzytanas <92881119+zytanas@users.noreply.github.com>
2023-04-27 16:13:57 +0800

    user input validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,67 +1,156 @@
-import React from 'react'
-import { Grid, Paper, TextField, Button, InputAdornment,Typography, Avatar, Box, Stack} from '@mui/material'
+import { Grid, Paper, TextField, Button, InputAdornment, Box, Stack } from '@mui/material'
 import LoginIcon from '@mui/icons-material/Login';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PasswordIcon from '@mui/icons-material/Password';
+import Logo from '../components/nms-logo/nms.png'
+import React, { useState } from 'react';
 
 const Login = () => {
-    const paperStyle = { padding: 40, height: '45vh', width: 350, borderRadius: '16px', borderColor:'borderColor.main',margin: 'auto', backgroundColor: '#191e33' }
-    
-    
-    return (
-        <Box sx={{ bgcolor: '#262626', height: '100vh', width: '100vw' }}>
-        <Grid>
-            <Paper elevation={5} style={paperStyle}>
-                <Grid align='center'>
-                <Avatar  variant='square' src='https://newmediaservices.com.au/wp-content/uploads/2019/02/NMS-LOGO-HD-2014_1.png'sx={{ width: 130, height: 56 }}>     
-                </Avatar>
-                    <h2>Welcome to NMS Kitchen Inventory System</h2>
-                    <h4 >SIGN IN</h4>
-                </Grid >
-                <Stack spacing={2}>
-                <TextField
-                label='Username' 
-                sx={{bgcolor:"#24262e"}}
-                InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <AccountCircleIcon />
-                      </InputAdornment>
-                    ),
-                  }} 
-                fullWidth />
-                
-                <TextField
-                label='Password' 
-                type='password' 
-                sx={{bgcolor:"#24262e"}} 
-                InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <PasswordIcon />
-                      </InputAdornment>
-                    ),
-                  }} 
-                fullWidth 
-                required />
-                
-                <Button type='submit' variant='contained'fullWidth 
-                sx={{
-                    fontWeight: 'bold',
-                    bgcolor: '#7a0702', ":hover": {
-                        bgcolor: "#450c0a",
-                        color: "white" 
-                    }
-                }}>
-                    Log In <LoginIcon />
-                </Button>
-                </Stack>
-                <Typography textAlign="center" fontSize={'14px'}>
-                </Typography>
-            </Paper>
-        </Grid>
-        </Box>
-    )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [usernameTouched, setUsernameTouched] = useState(false);
+  const [passwordTouched, setPasswordTouched] = useState(false);
 
-export default Login
+  const handleUsernameChange = (event) => {
+    const value = event.target.value.trim();
+    setUsername(value);
+    setUsernameError(false);
+    const re = /^[a-zA-Z0-9_]+$/;
+    if (value === '') {
+      setUsernameError(true);
+    } else if (value.length < 3 || value.length > 8) {
+      setUsernameError(true);
+    } else if (!re.test(value)) {
+      setUsernameError(true);
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    const value = event.target.value.trim();
+    setPassword(value);
+    setPasswordError(false);
+    if (value === '') {
+      setPasswordError(true);
+    } else if (value.length < 3 || value.length > 8) {
+      setPasswordError(true);
+    }
+  };
+
+  const handleUsernameBlur = () => {
+    setUsernameTouched(true);
+    if (username.trim() === '') {
+      setUsernameError(true);
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    setPasswordTouched(true);
+    if (password.trim() === '') {
+      setPasswordError(true);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validate username and password
+    const re = /^[a-zA-Z0-9_]+$/;
+    setUsernameError(
+      username.trim() === '' ||
+      username.length < 3 ||
+      username.length > 8 ||
+      !re.test(username)
+    );
+    setPasswordError(
+      password.trim() === '' ||
+      password.length < 3 ||
+      password.length > 8
+    );
+
+    // Submit the form if there are no errors
+    if (!usernameError && !passwordError) {
+      console.log('Submitting form...');
+    }
+  };
+
+
+  return (
+    <Box sx={{ bgcolor: '#262626', height: '100vh', width: '100vw' }}
+      noValidate
+      autoComplete="off">
+      <Grid>
+        <Paper elevation={1} sx={{ padding: 15, height: '40vh', width: 350, borderRadius: '16px', border: 2, borderColor: 'borderColor.main', margin: 'auto', backgroundColor: '#191e33' }}>
+          <Grid align='center'>
+            <img src={Logo} alt={Logo} width="120px" />
+            <h2>NMS Kitchen Inventory System</h2>
+            <h4 >SIGN IN</h4>
+          </Grid >
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={2}>
+              <TextField
+                label='Username'
+                value={username}
+                onChange={handleUsernameChange}
+                onBlur={handleUsernameBlur}
+                error={usernameError}
+                helperText={
+                  usernameError
+                    ? (usernameTouched ? 'Username is required and must be 3-8 characters long, and can only contain letters, numbers, and underscores' : 'Required')
+                    : ''
+                }
+
+                sx={{ bgcolor: "#24262e" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircleIcon />
+                    </InputAdornment>
+                  ),
+                }}
+
+                fullWidth
+                required />
+
+              <TextField
+                label='Password'
+                type='password'
+                value={password}
+                onChange={handlePasswordChange}
+                onBlur={handlePasswordBlur}
+                error={passwordError}
+                helperText={
+                  passwordError
+                    ? (passwordTouched ? 'Password is required and must be 3-8 characters long' : 'Required')
+                    : ''
+                }
+                sx={{ bgcolor: "#24262e" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PasswordIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                fullWidth
+                required />
+
+              <Button type='submit' variant='contained' fullWidth
+                sx={{
+                  fontWeight: 'bold',
+                  bgcolor: '#7a0702', ":hover": {
+                    bgcolor: "#450c0a",
+                    color: "white"
+                  }
+                }}>
+                Log In <LoginIcon />
+              </Button>
+            </Stack>
+          </form>
+        </Paper>
+      </Grid>
+    </Box>
+  )
+}
+export default Login;
